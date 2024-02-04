66881597f18813d4d0ab7fa105f7f694cb402c2d
LMNJIT <oneweirdonez@gmail.com>
2024-02-04 18:27:24 -0500

    Create home_page.php

---
+++ home_page.php
@@ -0,0 +1,10 @@
+<?php 
+?>
+<html> 
+    <head>
+        <title>Luka's Tech Shop</title>
+        </head>
+        <body>
+            <h1>We sell tech and tech accessories! Buy our premium wireless earbuds, laptop stands, portable phone chargers, bluetooth keyboards, and laptop backpacks!</h1>
+        </body>
+</html>

7e5880ddc775dac3c99039768783387a5463a515
LM <ldm29@njit.edu>
2024-02-05 13:43:25 -0500

    base shipping/home page

--- home_page.php
+++ home_page.php
@@ -5,6 +5,7 @@
         <title>Luka's Tech Shop</title>
         </head>
         <body>
-            <h1>We sell tech and tech accessories! Buy our premium wireless earbuds, laptop stands, portable phone chargers, bluetooth keyboards, and laptop backpacks!</h1>
+        <h1>Home Page</h1>
+        <h1>We sell tech and tech accessories! Buy our premium wireless earbuds, laptop stands, portable phone chargers, bluetooth keyboards, and laptop backpacks!</h1>
         </body>
 </html>
---
+++ shipping_page.php
@@ -0,0 +1,11 @@
+<?php 
+?>
+<html>
+    <head>
+        <title>Shipping Page</title>
+    </head>
+        <body>
+            <h1>Shipping Page</h1>
+        </body>
+</html>
+

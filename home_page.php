da8cff95730b56557d1406796cba0d786a10fdd1
LMNJIT <oneweirdonez@gmail.com>
2024-02-06 19:39:05 -0500

    Update home_page.php

--- home_page.php
+++ home_page.php
@@ -10,18 +10,22 @@
         <title>Home Page</title>
         <link rel="stylesheet" href="lukas_tech_shop.css"/>
         </head>
-        <body>
-        <h1>Home Page</h1>
-        <h2>Luka's Tech Shop</h2>
-        <h3>We sell tech and tech accessories! Buy some of our excellent products, such as...</h3>
-        <ul>
-            <li>Wireless Earbuds</li>
-            <li>Laptop Stands</li>
-            <li>Portable Phone Chargers</li>
-            <li>Bluetooth Keyboards</li>
-            <li>Laptop Backpacks</li>
-        </ul>
+        <main>
+            <body>
+            <h1>Home Page</h1>
+            <h2>Luka's Tech Shop</h2>
+            <h3>We sell tech and tech accessories! Buy some of our excellent products, such as...</h3>
+            <ul>
+                <li>Wireless Earbuds</li>
+                <li>Laptop Stands</li>
+                <li>Portable Phone Chargers</li>
+                <li>Bluetooth Keyboards</li>
+                <li>Laptop Backpacks</li>
+            </ul>
+        </main>
+        <footer>
         <a href="http://localhost/LMNJIT/git/IT202-ldm29-Project/shipping_page.php">Shipping Page</a>
+        </footer>
         </body>
 </html>
 

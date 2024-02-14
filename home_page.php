5e45fb6448e875ea6d1b5954dacbc9a7f7797a56
LMNJIT <oneweirdonez@gmail.com>
2024-02-14 01:04:17 -0500

    Started shipping page

--- home_page.php
+++ home_page.php
@@ -1,15 +1,6 @@
 <?php 
 ?>
 
-<header>
-    <img src="airpods.png" alt="html image" width="60"/>
-    <img src="shop_logo.png" alt="html image" width ="60"/>
-    <img src="laptop_backpack.png" alt="html image" width ="60"/>
-    <img src="laptop_stand.jpg" alt="html image" width ="60"/>
-    <img src="portable_phone_charger.png" alt="html image" width ="60"/>
-    <img src="bluetooth_keyboards.png" alt="html image" width ="60"/>
-</header>
-
 <html> 
         <head>
             <title>Home Page</title>
@@ -23,7 +14,7 @@
         <body>
             <h3> Looking for some tech to buy? You've come
                 to the right place! </h3>
-            <h3> Buy some of our excellent products, such as: </h3>
+            <h3> Purchase some of our excellent products, such as: </h3>
             <ul>
                 <li>Wireless Earbuds</li>
                 <li>Laptop Stands</li>
@@ -35,18 +26,31 @@
                 good, affordable tech and tech accessories. Stores were marking up prices and treating 
                 customers like they knew nothing about the products! The shopping environment these 
                 companies fosterd was becoming increasingly hostile to the average consumer. Luka's Tech 
-                Shop is dedicated to doing the exact opposite of this; our mission statement is to give 
+                Shop is dedicated to doing the exact opposite of this; our mission is to give 
                 our customers excellent tech products and tech accessories while treating them with the 
                 respect they deserve.</p>
         </main>
         <footer>
         <h4> Navigation </h4>
         <nav>
-            <a href="http://localhost/LMNJIT/git/IT202-ldm29-Project/shipping_page.php">Shipping Page</a>
+            <a href="http://localhost/LMNJIT/git/IT202-ldm29-Project/shipping_page.html">Shipping Page</a>
             <a href="http://localhost/LMNJIT/git/IT202-ldm29-Project/home_page.php">Home Page</a>
         </nav>
             <p>By Luka Mayer</p>
         </footer>
         </body>
+
+        <!-- Poppins Font from https://fonts.google.com/selection/embed -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 </html>
 
+<footer>
+    <img src="shop_logo.png" alt="html image" width ="60"/>
+    <img src="airpods.png" alt="html image" width="60"/>
+    <img src="laptop_backpack.png" alt="html image" width ="60"/>
+    <img src="laptop_stand.jpg" alt="html image" width ="60"/>
+    <img src="portable_phone_charger.png" alt="html image" width ="60"/>
+    <img src="bluetooth_keyboards.png" alt="html image" width ="60"/>
+</footer>
--- shipping_page.php
+++ shipping_page.php
@@ -1,21 +1,7 @@
-<?php 
+<?php
 
 ?>
-<html>
-<head>
-    <title>Shipping Page</title>
-        <link rel="stylesheet" href="lukas_tech_shop.css"/>
-        </head>
-        <body>
-            <h1>Shipping Page</h1>
-        </body>
-        <footer>
-        <h4> Navigation </h4>
-        <nav>
-            <a href="http://localhost/LMNJIT/git/IT202-ldm29-Project/shipping_page.php">Shipping Page</a>
-            <a href="http://localhost/LMNJIT/git/IT202-ldm29-Project/home_page.php">Home Page</a>
-        </nav>
-            <p>By Luka Mayer</p>
-        </footer>
-</html>
 
+<html>
+    
+</html>

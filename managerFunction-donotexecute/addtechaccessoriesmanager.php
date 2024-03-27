8b6457a6d7c5ddf7555ce640d795994b078aac12
LMNJIT <oneweirdonez@gmail.com>
2024-03-27 13:24:31 -0400

    small stuff

--- managerFunction-donotexecute/addtechaccessoriesmanager.php
+++ managerFunction-donotexecute/addtechaccessoriesmanager.php
@@ -9,6 +9,25 @@ Version 1.0
 -->
 
 <?php
+    require_once('database_njit.php');
+
+    function getDB() {
+        $dsn = 'mysql:host=sql1.njit.edu;port=3306;dbname=ldm29';
+        $username = 'ldm29';
+        $password = 'IT202mySQL@';
+    
+        try {
+            $db = new PDO($dsn, $username, $password);
+            echo '<p>You are connected to the local database.</p>';
+        } catch(PDOException $ex) {
+            // -> equivalent of . in x.length() in java
+            $error_message = $ex->getMessage();
+            include('database_error.php');
+            exit();
+        }
+    return $db;
+    }
+
     function addtechaccesoriesmanager($email, $password, $firstName, $lastName) {
         $db = getDB();
         $hash = password_hash($password, PASSWORD_DEFAULT);
@@ -22,6 +41,10 @@ Version 1.0
         $statement->execute();
         $statement->closeCursor();
     }
+
+    addtechaccesoriesmanager('luka@lukastechaccessories.com','pass123','Luka','Mayer');
+    addtechaccesoriesmanager('maurice@lukastechaccessories.com','mauriceThegeese','Maurice','Geese');
+    addtechaccesoriesmanager('diocletian@lukastechaccessories.com','dioDiodio','Diocletian','III');
 ?>
 
 <!--
